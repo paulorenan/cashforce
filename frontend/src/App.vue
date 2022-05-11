a7c6488e5fd6a9de8522024337c32bcb5d7b57d2
Paulo Renan <able.paulorenan@gmail.com>
2022-05-10 22:00:25 -0300

    começando front-end

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,60 +1,31 @@
 <template>
-  <v-app>
-    <v-app-bar
+  <v-app id="inspire">
+    <v-navigation-drawer
+      color="#FBFDFE"
+      v-model="drawer"
       app
-      color="primary"
-      dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
+      <!--  -->
+    </v-navigation-drawer>
+    <v-app-bar app elevation="1" />
 
     <v-main>
-      <HelloWorld/>
+      <!--  -->
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+
   },
 
   data: () => ({
-    //
+    drawer: true,
   }),
 };
 </script>
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,17 +1,23 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-  },
-  getters: {
+    orders: [],
   },
   mutations: {
+    getOrders(state, order) {
+      state.orders = order
+    }
   },
   actions: {
+    getOrders({ commit }) {
+      axios.get('http://localhost:3001/orders').then(res => {
+        commit('getOrders', res.data)
+      });
+    }
   },
-  modules: {
-  }
 })
